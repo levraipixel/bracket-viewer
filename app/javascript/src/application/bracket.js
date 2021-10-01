25755b19cb56ead5c7d1916aef438680c53998cc
Yann Hourdel <yann@hourdel.fr>
2021-10-01 22:06:43 +0200

    add lines

--- app/javascript/src/application/bracket.js
+++ app/javascript/src/application/bracket.js
@@ -6,16 +6,49 @@ const fetchBracket = (bracketUrl, callback) => {
   $.getJSON(bracketUrl, callback);
 };
 
-const renderMatch = (match) => {
+const renderMatch = (match, matchIdx, bracket, roundIdx) => {
   let $result = $('<div>').addClass('match');
-  match.forEach((player, index) => {
+  if(matchIdx < 1) {
+    $result.addClass('first');
+  }
+  match.forEach((player, playerIdx) => {
     let $player = $('<div class="player">');
-    $player.append($('<div class="name">').html(player[0]));
+    let playerName = player[0];
+    if(roundIdx > 0 && playerName) {
+      let previousRound = bracket[roundIdx - 1];
+      let $from = $('<div class="from">');
+
+      if(playerIdx < 1) {
+        if(matchIdx < 1) {
+          let playerPreviousMatch = previousRound[matchIdx];
+          console.log('previous match for', playerName, 'is', playerPreviousMatch);
+          if(playerName == playerPreviousMatch[1][0]) {
+            $player.addClass('from-lower');
+          }
+        } else {
+          if(matchIdx > 0) {
+            let playerPreviousMatch = previousRound[matchIdx - 1];
+            if(playerName == playerPreviousMatch[0][0]) {
+              $player.addClass('from-higher');
+            }
+          }
+        }
+      } else {
+        if(matchIdx < (previousRound.length - 1)) {
+          let playerPreviousMatch = previousRound[matchIdx + 1];
+          if(playerName == playerPreviousMatch[1][0]) {
+            $player.addClass('from-lower');
+          }
+        }
+      }
+      $player.append($from);
+    }
+    $player.append($('<div class="name">').html(playerName));
     $player.append($('<div class="score">').html(player[1]));
-    if(match[index][1] > match[(1 + index) % 2][1]) {
+    if(match[playerIdx][1] > match[(1 + playerIdx) % 2][1]) {
       $player.addClass('winner');
     } else {
-      if(match[index][1] < match[(1 + index) % 2][1]) {
+      if(match[playerIdx][1] < match[(1 + playerIdx) % 2][1]) {
         $player.addClass('loser');
       }
     }
@@ -28,9 +61,9 @@ const renderBracket = (bracket) => {
   let $result = $('<div>').addClass('bracket');
   bracket.forEach((round, roundIdx) => {
     let $round = $('<div>').addClass('round');
-    $round.append($('<h3>').html('Round ' + roundIdx));
-    round.forEach(match => {
-      $round.append(renderMatch(match));
+    $round.append($('<h3>').html('Tour ' + (1+roundIdx)));
+    round.forEach((match, matchIdx) => {
+      $round.append(renderMatch(match, matchIdx, bracket, roundIdx));
     });
     $result.append($round);
   });
@@ -66,7 +99,7 @@ const initElement = (el) => {
   };
 
   update();
-  // setInterval(update, 30 * 1000);
+  setInterval(update, 30 * 1000);
 };
 
 $(document).ready(() => {
